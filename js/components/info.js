9c849e872408895bb6cfe9a9314edad572a4be8e
ADEP-123 <andep210399@gmail.com>
2023-09-19 17:10:25 -0500

    feat: :sparkles: componentes

---
+++ js/components/info.js
@@ -0,0 +1,18 @@
+const infoHome = /*html*/`
+<h1>Bienvenido</h1>
+<p style="text-align: justify;">En esta pagina web podras encontrar ejercicios de programacion realizados
+    durante el desarrollo de la materia <i>programacion 1</i> en la Universidad de Santander (UDES) en lenguaje C++.
+</p>
+<h5 style="text-align: left;">Instrucciones:</h5>
+<p style="text-align: justify;">
+    En el panel superior de esta aplicacion podra encontrar el boton con titulo "Menu" dando click a este
+    boton desplegará una lista en la parte derecha en la cual dando click a alguno de los ejercicios
+    depsplegará en este apartado la informacion del ejercicio, para volver a este menu en cualquier momento
+    puede dar click en el boton home (el boton con forma de casa) en el panel superior izquierdo.
+</p>
+<div id="contact_info_div">
+    <span class="material-icons" id="img_profile">account_circle</span>
+    <p id="contact_info_text"><b>Andrés David Elizalde Peralta</b><br><i>01220371068</i></p>
+</div>
+`
+export default infoHome 
---
+++ js/infoSelect.js
@@ -0,0 +1,58 @@
+import infoHome from "./components/info.js";
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const info = document.getElementById("info")
+    info.innerHTML = infoHome
+    const homebutton = document.getElementById("home_button_link")
+    const sidebarButtons = document.querySelectorAll("#sidebar button");
+
+    sidebarButtons.forEach((button) => {
+
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            const moduleName = `ej${button.value}`;
+
+
+            import(`./components/${moduleName}.js`)
+                .then((modul) => {
+                    info.innerHTML = modul.default;
+                    Prism.highlightAll();
+
+                    const copyButtons = document.querySelectorAll('.copy-button');
+                    const codeBlocks = document.querySelectorAll('.code-container');
+                    copyButtons.forEach((button, index) => {
+                        button.addEventListener('click', () => {
+
+                            const codeBlock = codeBlocks[index].querySelector('code');
+                            const tempTextArea = document.createElement('textarea');
+                            tempTextArea.value = codeBlock.textContent;
+                            document.body.appendChild(tempTextArea);
+                            tempTextArea.select();
+                            document.execCommand('copy');
+                            document.body.removeChild(tempTextArea);
+                            button.textContent = 'Copiado';
+                        });
+                    });
+
+                })
+                .catch((error) => {
+                    console.error({ error });
+                    info.innerHTML = /*html*/`
+                    <h1>Estamos trabajando en esta seccion</h1>
+                    <img src="./../imgs/robotError.gif" id="img_res">
+                    `;
+                });
+
+        });
+    });
+
+    homebutton.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        info.innerHTML = infoHome
+    })
+
+});
