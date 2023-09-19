8327d06d61b41be0d8ac2b46fd3ab59d5c38478e
ADEP-123 <andep210399@gmail.com>
2023-09-19 17:42:50 -0500

    feat: :sparkles: ej 23 24

---
+++ js/components/ej23.js
@@ -0,0 +1,50 @@
+const ej23 = /*html*/`
+<h1>Ejercicio 23</h1>
+<p>Imprimiendo numeros con condicion 1 <= 100</p>
+<div id="codigo">
+<pre class="code-container">
+    <button class="copy-button">Copiar</button>
+    <code class="language-cpp">
+    #include <cmath>
+    #include <iostream>
+    #include <string>
+    using namespace std;
+    
+    int main() {
+      cout << "Tabla de Números del 1 al 100" << endl;
+    
+      // Imprimir encabezados de columna
+      cout << "+-----------+" << endl;
+      cout << "|  1 <= 100 |" << endl;
+      cout << "+-----------+" << endl;
+    
+      // Imprimir números del 1 al 100 en la tabla
+      for (int i = 1; i <= 100; i++) {
+        // Imprimir fila de datos
+        if (i < 10) {
+          cout << "| " << i << "        | ";
+        } else if (i < 100) {
+          cout << "| " << i << "       | ";
+        } else {
+          cout << "| " << i << "      | ";
+        }
+    
+        cout << std::endl;
+      }
+    
+      // Imprimir línea inferior de la tabla
+      cout << "+----------+" << endl;
+    
+      return 0;
+    }
+    </code>
+</pre>
+</div>
+<div id=div_img_res>
+    <h5>Resultado</h5>
+    <img src="./../imgs/ej23.png" id="img_res">
+    <img src="./../imgs/ej23_2.png" id="img_res">
+</div>
+
+`;
+export default ej23
