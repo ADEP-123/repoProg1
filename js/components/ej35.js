d9a455e29cfb20d624f5be8d86e9f3feefaa58d9
ADEP-123 <andep210399@gmail.com>
2023-09-26 14:35:04 -0500

    feat: :sparkles: ej35

---
+++ js/components/ej35.js
@@ -0,0 +1,29 @@
+const ej35 = /*html*/`
+<h1>Ejercicio 35</h1>
+<p>Codigo que convierte a codigo ASCII</p>
+<div id="codigo">
+<pre class="code-container">
+    <button class="copy-button">Copiar</button>
+    <code class="language-cpp">
+    #include <iostream>
+
+    using namespace std;
+    
+    int main() {
+      for (int i = 64; i < 122; i++) {
+        cout << char(i) << "\n";
+      }
+    //El codigo conbierte a codico ASCII
+      return 0;
+    }
+    </code>
+</pre>
+</div>
+<div id=div_img_res>
+    <h5>Resultado</h5>
+    <img src="./../imgs/ej35.png" id="img_res">
+    <!-- <img src="https://adepfolder.000webhostapp.com/Programacion%201/imgs/ej35.png" id="img_res"> -->
+</div>
+
+`;
+export default ej35
