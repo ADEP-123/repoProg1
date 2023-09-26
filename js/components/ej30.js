dc75efd46d80c7b8dbf3ea005a98ecdf6a0a7fcf
ADEP-123 <andep210399@gmail.com>
2023-09-26 14:07:32 -0500

    feat: :sparkles: ej30

---
+++ js/components/ej30.js
@@ -0,0 +1,56 @@
+const ej30 = /*html*/`
+<h1>Ejercicio 30</h1>
+<p>Tabla de multiplicar</p>
+<div id="codigo">
+<pre class="code-container">
+    <button class="copy-button">Copiar</button>
+    <code class="language-cpp">
+    #include <iostream>
+
+    using namespace std;
+    
+    int main() {
+      int numero;
+    
+      cout << "Ingrese un número para obtener su tabla de multiplicar: ";
+      cin >> numero;
+    
+      cout << "Tabla de Multiplicar del " << numero << ":" << endl;
+      cout << "+------------+------------+" << endl;
+      cout << "|   Numero   |  Resultado |" << endl;
+      cout << "+------------+------------+" << endl;
+    
+      for (int i = 1; i <= 10; i++) {
+        int resultado = numero * i;
+        if(i<10){
+           cout << "|   " << numero << " x " << i << "    |    " << resultado;
+        }else{
+          cout << "|   " << numero << " x " << i << "   |    " << resultado;
+        }
+       
+        
+    
+        // Alineación de los resultados para que la tabla sea uniforme
+        if (resultado < 10) {
+          cout << "       |" << endl;
+        } else {
+          cout << "      |" << endl;
+        }
+      }
+    
+      cout << "+------------+------------+" << endl;
+    
+      return 0;
+    }
+    
+    </code>
+</pre>
+</div>
+<div id=div_img_res>
+    <h5>Resultado</h5>
+    <img src="./../imgs/ej30.png" id="img_res">
+    <!-- <img src="https://adepfolder.000webhostapp.com/Programacion%201/imgs/ej30.png" id="img_res"> -->
+</div>
+
+`;
+export default ej30
