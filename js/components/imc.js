ff74bd8f4d4a866526b63397b95c8dd761643768
ADEP-123 <andep210399@gmail.com>
2023-10-07 20:44:53 -0500

    feat: :sparkles: imc

---
+++ js/components/imc.js
@@ -0,0 +1,26 @@
+const imc = /*html*/`
+<h1>Calculadora de Indice de Masa Corporal (IMC)</h1>
+<div id=div_info>
+<img src="./../imgs/imc.png" id="img_imc">
+<!--<img src="https://adepfolder.000webhostapp.com/Programacion%201/imgs/imc.png" id="img_res"> -->
+<p>Una medida de la obesidad se determina mediante el índice de masa corporal (IMC), que se calcula dividiendo los kilogramos de peso por el cuadrado de la estatura en metros (IMC = peso (kg)/ [estatura (m)]2. Según el Instituto Nacional del Corazón, los Pulmones y la Sangre de los Estados Unidos (NHLBI), el sobrepeso se define como un IMC de más de 25. Se considera que una persona es obesa si su IMC es superior a 30. Usted puede determinar su IMC con la calculadora que se encuentra a continuación. Con la cifra del IMC puede averiguar su composición corporal en la tabla que aparece debajo de la calculadora.</p>
+</div>
+
+<div id=calculadora>
+    <form id="formularioIMC">
+        <h4>Calculadora</h4>
+        <span>Estatura: </span>
+        <input type="text" id="estatura" name="estatura" placeholder="metros">
+        <span>Peso: </span>
+        <input type="text" id="peso" name="peso" placeholder="kilogramos"><br>
+        <button type="submit" id="submit_form">Calcular</button>
+    </form>
+    <br>
+    <spam>IMC: </spam>
+    <input type="number" id="result" placeholder="resultado" readOnly><br>
+</div>
+
+
+
+`;
+export default imc
--- js/infoSelect.js
+++ js/infoSelect.js
@@ -6,14 +6,67 @@ document.addEventListener("DOMContentLoaded", function () {
     info.innerHTML = infoHome
     const homebutton = document.getElementById("home_button_link")
     const sidebarButtons = document.querySelectorAll("#sidebar button");
+    let eventExist = false
+    info.addEventListener("click", (event) => {
+        event.stopPropagation()
+        const formularioIMc = document.getElementById("formularioIMC");
+        if (formularioIMc) {
+            if (eventExist == false) {
+                formularioIMc.addEventListener("submit", (event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    const estatura = document.getElementById("estatura")
+                    const peso = document.getElementById("peso")
+                    let ok = true;
+                    if (isNaN(estatura.value) == true) {
+                        alert("la estatura debe ser un numero")
+                        estatura.value = null
+                        ok = false
+                    }
+                    if (isNaN(peso.value)) {
+                        alert("El peso debe ser un numero")
+                        peso.value = null
+                        ok = false
+                    }
+                    const estaturaValue = Number(estatura.value)
+                    const pesoValue = Number(peso.value)
+                    if (pesoValue == 0) {
+                        alert("El peso no puede ser 0")
+                        peso.value = null
+                        ok = false
+                    }
+                    if (estaturaValue < 0 || pesoValue < 0) {
+                        alert("Los valores deben ser positivos")
+                        estatura.value = null
+                        peso.value = null
+                        ok = false
+                    }
+                    if (ok == true) {
+                        const result = document.getElementById("result")
+                        result.value = pesoValue / (estaturaValue * estaturaValue)
+                    }
+
+                });
+            }
+
+        }
+        eventExist = true
+    })
 
     sidebarButtons.forEach((button) => {
 
         button.addEventListener("click", (e) => {
             e.preventDefault();
             e.stopPropagation();
+            let moduleName;
+            if (button.value == "imc") {
+                moduleName = `${button.value}`;
+            } else {
 
-            const moduleName = `ej${button.value}`;
+                moduleName = `ej${button.value}`;
+            }
+
 
 
             import(`./components/${moduleName}.js`)
