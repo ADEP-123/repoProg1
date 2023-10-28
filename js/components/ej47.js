74e86c5b4554bd68d42db106834c9afdee9ee612
ADEP-123 <andep210399@gmail.com>
2023-10-28 12:19:24 -0500

    feat: :sparkles: ej47

---
+++ js/components/ej47.js
@@ -0,0 +1,44 @@
+const ej47 = /*html*/`
+<h1>Ejercicio 7 While</h1>
+<div id="codigo">
+<pre class="code-container">
+    <button class="copy-button">Copiar</button>
+    <code class="language-cpp">
+        <script type="text/plain">
+            #include <iostream>
+            using namespace std;
+            
+            int main() {
+              cout << "Funcionamiento de las posiciones de un array" << endl;
+              cout << endl;
+            
+              int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            
+              cout << "Aqui vamos a mostrar los valores en cada posicion"<< endl;
+            
+              cout << "Posicion 0: " << array[0] << endl;
+              cout << "Posicion 1: " << array[1] << endl;
+              cout << "Posicion 2: " << array[2] << endl;
+              cout << "Posicion 3: " << array[3] << endl;
+              cout << "Posicion 4: " << array[4] << endl;
+              cout << "Posicion 5: " << array[5] << endl;
+              cout << "Posicion 6: " << array[6] << endl;
+              cout << "Posicion 7: " << array[7] << endl;
+              cout << "Posicion 8: " << array[8] << endl;
+              cout << "Posicion 9: " << array[9] << endl;
+            
+              return 0;
+            }
+        </script>
+    </code>
+</pre>
+</div>
+<div id=div_img_res>
+    <h5>Resultado</h5>
+    <img src="./../imgs/ej47.png" id="img_res">
+
+    <!-- <img src="https://adepfolder.000webhostapp.com/Programacion%201/imgs/ej47.png" id="img_res"> -->
+</div>
+
+`;
+export default ej47
