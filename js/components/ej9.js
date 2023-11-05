de2fbb39967da0ecbb5b9921195b2ab97c30293b
ADEP-123 <andep210399@gmail.com>
2023-11-05 15:48:30 -0500

    feat: :sparkles: ej9

---
+++ js/components/ej9.js
@@ -0,0 +1,46 @@
+import config from "../config.js";
+const ej9 = /*html*/`
+<h1>Ejercicio 9</h1>
+<p>Programa que reciba ambos lados de un triangulo y calcule la hipotenusa </p>
+<div id="codigo">
+<pre class="code-container">
+    <button class="copy-button">Copiar</button>
+    <code class="language-cpp">
+    <script type="text/x-c++src">
+      #include <iostream>
+      #include <cmath>
+      
+      using namespace std;
+      
+      int main() {
+          // Declaración de variables
+          double lado1, lado2, hipotenusa;
+      
+          // Entrada de datos
+          cout << "Ingrese la longitud del primer lado: ";
+          cin >> lado1;
+      
+          cout << "Ingrese la longitud del segundo lado: ";
+          cin >> lado2;
+      
+          // Cálculo de la hipotenusa usando el teorema de Pitágoras
+          hipotenusa = sqrt(pow(lado1, 2) + pow(lado2, 2));
+      
+          // Salida del resultado
+          cout << "La longitud de la hipotenusa es: " << hipotenusa << endl;
+      
+          return 0;
+      }
+
+    </script>
+    </code>
+</pre>
+</div>
+<div id=div_img_res>
+    <h5>Resultado</h5>
+    <img src="./../imgs/ej9.png" id="img_res">
+    <!--<img src="https://adepfolder.000webhostapp.com/Programacion%201/imgs/ej9.png" id="img_res"> -->
+</div>
+
+`;
+export default ej9
