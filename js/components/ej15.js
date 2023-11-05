a3aa2d03ae8075ee8aad3097d11af6c9a102b1b4
ADEP-123 <andep210399@gmail.com>
2023-11-05 16:23:28 -0500

    feat: :sparkles: ej15

--- js/components/ej15.js
+++ js/components/ej15.js
@@ -1,6 +1,6 @@
 import config from "../config.js";
-const ej14 = /*html*/`
-<h1>Ejercicio 14</h1>
+const ej15 = /*html*/`
+<h1>Ejercicio 15</h1>
 <p>Diseñar un algoritmo que identifique si un año es bisiesto <br>
 Basados en la información en línea un año bisiesto es aquel que es divisible por 4 pero no por 100 o es divisible por 4, por 100 y por 400. </p>
 <div id="codigo">
@@ -10,7 +10,7 @@ Basados en la información en línea un año bisiesto es aquel que es divisible 
     <script type="text/x-c++src">
       #include <cmath> 
       #include <iostream> 
-      
+
       using namespace std; 
       int main() { 
       
@@ -18,7 +18,7 @@ Basados en la información en línea un año bisiesto es aquel que es divisible 
         int n; 
       
         // Asignacion de variables 
-        cout << "Ingrese el año de interes\n "; 
+        cout << "Ingrese el año de interes: "; 
         cin >> n; 
       
         if (n % 4 == 0) { 
@@ -43,9 +43,9 @@ Basados en la información en línea un año bisiesto es aquel que es divisible 
 </div>
 <div id=div_img_res>
     <h5>Resultado</h5>
-    <img src="./../imgs/ej14.png" id="img_res">
-    <!--<img src="https://adepfolder.000webhostapp.com/Programacion%201/imgs/ej14.png" id="img_res"> -->
+    <img src="./../imgs/ej15.png" id="img_res">
+    <!--<img src="https://adepfolder.000webhostapp.com/Programacion%201/imgs/ej15.png" id="img_res"> -->
 </div>
 
 `;
-export default ej14
+export default ej15
